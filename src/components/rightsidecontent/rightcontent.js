2022e37dd7280c65f48e92a9fe32c7152a0fda56
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-29 16:23:41 -0400

    react push

---
+++ src/components/rightsidecontent/rightcontent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./rightcontent.module.css"
+
+function Rightcontent() {
+    return (
+        <div className={style.outer}>
+            <div className="inner">
+
+            </div>
+            
+        </div>
+    )
+}
+export default Rightcontent;
