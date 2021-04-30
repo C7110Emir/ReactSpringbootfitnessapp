2b99e6d5d03213edd9b6cf9dabe5aefde00aae96
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-29 22:33:28 -0400

    loginsignup start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import FirstHomePage from "./components/FirstHomePage.js"
+import LoginSignup from "./components/LoginSignup.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FirstHomePage />
+        <LoginSignup />
+        
       </header>
     </div>
   );
