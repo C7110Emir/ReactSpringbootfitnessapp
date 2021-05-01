60510d51ec82d210d22a0225f19954f0cc9d0c5f
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-01 17:12:18 -0400

    edits

--- src/components/loginform/loginform.js
+++ src/components/loginform/loginform.js
@@ -15,20 +15,20 @@ function Loginform() {
                     <h1 className={style.logintitle}>Log In</h1><br/>
                     <hr/><br/><br/>
                     <label for="username" className={style.loginusername} className={style.text}>Username:</label><br/><br/>
-                    <input type="text" id="username" className={style.loginusernameinput} placeholder="Username"/><br/><br/><br/><br/>
+                    <input type="text" id="username" className={style.loginusernameinput} className={style.logininput} placeholder="Username"/><br/><br/><br/><br/>
                     <label for="loginpassword"  className={style.loginpassword} className={style.text}>Password:</label><br/><br/>
-                    <input type="password" id="loginpassword" className={style.loginpassword} maxLength="6" placeholder="Password"/><br/><br/><br/>
-                    <p className={style.loginnoaccount}>You don't have an account?</p><br/><br/>
+                    <input type="password" id="loginpassword" className={style.loginpassword} className={style.logininput}  maxLength="6" placeholder="Password"/><br/><br/><br/>
+                    <p className={style.loginnoaccount} id={style.youdonthaveanaccount}>You don't have an account?</p><br/><br/>
                     
 
 
-                    <button type="submit" disabled>Log In</button> 
+                    <button type="submit" className={style.loginbutton} >Log In</button> 
                 </form>          
             </div> 
             <div className={style.SignUpdiv}>
-                <form action="" className={style.signupform}>
+                <form action="" className={style.signupform} onSubmit={submitButton}>
                     <h1 className={style.signuptitle}>Sign Up</h1><br/>
-                    <hr></hr><br/><br/>
+                    <hr></hr><br/>
                     <label for="signupusername" className={style.signupusername} className={style.text}>Username:</label><br/><br/>
                     <input type="username" id="signupusername" className={style.signupusernameinput} className={style.input} placeholder="Username"/><br/><br/>
                     <label for="signuppassword" className={style.signuppassword} className={style.text}>Password:</label><br/><br/>
@@ -39,7 +39,7 @@ function Loginform() {
                     <input type="email" id="signupemail" className={style.signupemailinput} className={style.input} placeholder="example@example.com"/><br/><br/>
                     <label for="signupcountry" className={style.signupcountry} className={style.text}>Country:</label><br/><br/>
                     <input type="country" id="signupcountry" className={style.signupcountryinput} className={style.input} placeholder="Country"/><br/><br/>
-                    <button type="submit" className={style.Signupbutton} disabled>Sign Up</button>
+                    <button type="submit" className={style.Signupbutton} >Sign Up</button>
                 </form>
             </div>
 
